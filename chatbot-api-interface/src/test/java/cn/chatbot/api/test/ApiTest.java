fafb1509293093996895a6fc39dfbe8ab1ee320b
work <1478605454@qq.com>
2023-09-10 10:16:49 +0800

    Initial commit

---
+++ chatbot-api-interface/src/test/java/cn/chatbot/api/test/ApiTest.java
@@ -0,0 +1,32 @@
+package cn.chatbot.api.test;
+
+import cn.chatbot.api.ApiApplication;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description 单元测试
+ * @github https://github.com/fuzhengwei
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+public class ApiTest {
+//单元测试
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+
+    }
+}
